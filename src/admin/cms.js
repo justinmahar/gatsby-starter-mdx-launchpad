6d3449000b79c7e533f34b2a54c2aefbdadf8dfa
Dev Boldly <devboldly@gmail.com>
2020-02-02 11:10:07 -0600

    Add output

--- src/admin/cms.js
+++ src/admin/cms.js
@@ -3,6 +3,8 @@ import blogPosts from './collections/blog-posts.json';
 import pages from './collections/pages.json';
 import settings from './collections/settings';
 
+console.log('CMS init');
+
 const setup = {
   backend: {
     name: 'git-gateway',
