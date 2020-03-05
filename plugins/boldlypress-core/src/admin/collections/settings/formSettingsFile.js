b5e1d75e072ed059d428d0b663c5932eb7451d1c
Dev Boldly <devboldly@gmail.com>
2020-03-05 10:51:04 -0600

    Update form settings file

--- plugins/boldlypress-core/src/admin/collections/settings/formSettingsFile.js
+++ plugins/boldlypress-core/src/admin/collections/settings/formSettingsFile.js
@@ -49,7 +49,29 @@ const formSettingsFile = {
         {
           label: 'Form Async Request Mode',
           name: 'formAsyncRequestMode',
-          widget: 'string',
+          widget: 'select',
+          options: [
+            {
+              label:
+                'same-origin - If a request is made to another origin with this mode set, the result is simply an error. You could use this to ensure that a request is always being made to your origin.',
+              value: 'same-origin',
+            },
+            {
+              label:
+                'no-cors - Prevents the method from being anything other than HEAD, GET or POST, and the headers from being anything other than simple headers. If any ServiceWorkers intercept these requests, they may not add or override any headers except for those that are simple headers. In addition, JavaScript may not access any properties of the resulting Response. This ensures that ServiceWorkers do not affect the semantics of the Web and prevents security and privacy issues arising from leaking data across domains.',
+              value: 'no-cors',
+            },
+            {
+              label:
+                'cors - Allows cross-origin requests, for example to access various APIs offered by 3rd party vendors. These are expected to adhere to the CORS protocol. Only a limited set of headers are exposed in the Response, but the body is readable.',
+              value: 'cors',
+            },
+            {
+              label:
+                'navigate - A mode for supporting navigation. The navigate value is intended to be used only by HTML navigation. A navigate request is created only while navigating between documents.',
+              value: 'navigate',
+            },
+          ],
           default: 'cors',
           hint:
             'The mode you want to use for the request, e.g., cors, no-cors, same-origin, or navigate. The default is cors. Use no-cors for services like Google Forms.',
